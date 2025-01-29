327a3a1335e283e81452fd46f54524e04c47df8d
CodeJuuun <Chomseng.a@gmail.com>
2025-01-29 16:53:01 -0500

    linted file

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const morgan = require('morgan');
-const app = express();
-const PORT = 8080; //default port 8080
+const morgan  = require('morgan');
+const app     = express();
+const PORT    = 8080; //default port 8080
 
 app.set("view engine", "ejs");
 
@@ -10,23 +10,23 @@ app.set("view engine", "ejs");
 app.use(morgan('dev'));
 //---------------------------------------------------------
 //short url generator
-function generateRandomString() {
-  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  let randomStr = ""
+const generateRandomString = () => {
+  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomStr = "";
 
   for (let i = 0; i < 6; i++) {
     const index = Math.floor(Math.random() * char.length);
     randomStr += char[index];
   }
   return randomStr;
-}
+};
 //---------------------------------------------------------
 // key is short URL, value is long URL
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  b2xVn2  : "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
- //this code needs to be before ALL routes so it can parse any incoming data into something readable
+//this code needs to be before ALL routes so it can parse any incoming data into something readable
 app.use(express.urlencoded({ extended: true }));
 
 //---------------------------------------------------------
@@ -42,7 +42,7 @@ app.get("/urls.json", (req, res) => {
 //------------
 app.get("/urls", (req, res) => {
   // need to send variables via inside object
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase
   };
   // parameters: templateName, variableName
@@ -62,7 +62,7 @@ app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
 
-  if(longURL) { //if url exists, create templateVars and render the template
+  if (longURL) { //if url exists, create templateVars and render the template
     const templateVars = {
       id: req.params.id,
       longURL: longURL
@@ -80,27 +80,27 @@ app.get("/u/:id", (req, res) => {
   if (longURL) {
     res.redirect(longURL);
   } else {
-     res.status(404).send("short URL not found");
+    res.status(404).send("short URL not found");
   }
-})
+});
 
 
 //---------------------------------------------------------
-//POST 
+//POST
 //---------------------------------------------------------
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL; // extract URL from body of req
-  urlDatabase[shortURL] = longURL // assign longURL the id from shortURL generated from function
+  urlDatabase[shortURL] = longURL;// assign longURL the id from shortURL generated from function
 
   console.log(`New URL added: ${longURL} as ${shortURL}`); // Log the POST request body to the console
   res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
 //------------
 app.post("/urls/:id/delete", (req, res) => {
-  const shortURL = req.params.id
+  const shortURL = req.params.id;
 
-  if(!urlDatabase[shortURL]) {
+  if (!urlDatabase[shortURL]) {
     return res.status(404).send("URL not found");
   }
 
@@ -110,20 +110,20 @@ app.post("/urls/:id/delete", (req, res) => {
 
 //------------
 app.post("/urls/:id", (req, res) => {
-  const shortURL = req.params.id; // capture shortURL from URL parameter
-  const updatedLongURL= req.body.longURL;
+  const shortURL        = req.params.id; // capture shortURL from URL parameter
+  const updatedLongURL  = req.body.longURL;
 
   // validation check
-  if(!updatedLongURL) {
+  if (!updatedLongURL) {
     return res.status(404).send("shortURL not found");
   }
 
-  if(urlDatabase[shortURL]) {
+  if (urlDatabase[shortURL]) {
     urlDatabase[shortURL] = updatedLongURL;
     console.log(`Updated URL: ${shortURL} to ${updatedLongURL}`);
     res.redirect("/urls");
-  };
-})
+  }
+});
 //---------------------------------------------------------
 app.listen(PORT, () => { // the code is what gets express app to start running
   console.log(`Example app listening on port ${PORT}!`);
