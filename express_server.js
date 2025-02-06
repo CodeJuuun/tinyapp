8745b4a8723039cee2364720ac27aff9a141b822
CodeJuuun <Chomseng.a@gmail.com>
2025-02-05 22:54:54 -0500

    added log in check to GET /urls/id

--- express_server.js
+++ express_server.js
@@ -155,6 +155,9 @@ app.get("/urls/new", (req, res) => {
 //---------------------------------------------------------
 // Route to display the short URL along with the original long URL
 app.get("/urls/:id", (req, res) => {
+  if (!req.user) {
+    return res.status(403).send("<h2>You must be logged in to view URL details.")
+  }
   const shortURL = req.params.id;
   const urlData = urlDatabase[shortURL]; // capture URL data
 
