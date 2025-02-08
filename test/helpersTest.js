a5b5d3a75084c3566fd1689776ac87e8791b1cf1
CodeJuuun <Chomseng.a@gmail.com>
2025-02-08 00:47:42 -0500

    added tests based on LLM

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -28,5 +28,46 @@ describe('getUserByEmail', function() {
     const user = getUserByEmail("nonexistent@example.com", testUsers);
     assert.isUndefined(user, "return value should be undefined if email does not exist");
   });
-
 });
+
+
+describe("urlsForUser", function () {
+// testing urlDatabase
+  const urlDatabase = {
+    "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "user1" },
+    "9sm5xK": { longURL: "http://www.google.com", userId: "user2" },
+    "abc123": { longURL: "http://www.example.com", userId: "user1" },
+    "xyz789": { longURL: "http://www.bing.com", userId: "user3" },
+  };
+
+// Define expectedOutput
+  it("should return URLs that belong to the given user", function () {
+    const user1Urls = urlsForUser("user1", urlDatabase);
+    const expectedOutput = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "user1" },
+      "abc123": { longURL: "http://www.example.com", userId: "user1" },
+    };
+    assert.deepEqual(user1Urls, expectedOutput);
+  })
+
+  // call the function
+  it("should return an empty object if the user has no URLs", function () {
+    const user4Urls = urlsForUser("user4", urlDatabase);
+    assert.deepEqual(user4Urls, {});
+  });
+
+  it("should return an empty object if the urlDatabase is empty", function () {
+    const emptyDatabase = {};
+    const result = urlsForUser("user1", emptyDatabase);
+    assert.deepEqual(result, {});
+  });
+
+  it("should not return URLs that belong to a different user", function () {
+    const user1Urls = urlsForUser("user1", urlDatabase);
+    
+    // Ensure that URLs belonging to other users are NOT included
+    assert.notProperty(user1Urls, "9sm5xK"); // Belongs to user2
+    assert.notProperty(user1Urls, "xyz789"); // Belongs to user3
+  });
+});
+
